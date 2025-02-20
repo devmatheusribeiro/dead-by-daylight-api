b4a17539e7f3f38d2c3cacdef549aa9b1ec29abc
Matheus Silva <github.matheusribeiro@gmail.com>
2025-02-20 20:42:24 -0300

    initial settings

---
+++ src/test/java/com/deadybydaylight/DbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deadybydaylight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
